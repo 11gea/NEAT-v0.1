1a1b9978497ca2fe894320c6af66993abeab8df2
11gea <21geajustin@gmail.com>
2020-04-14 19:04:46 +0000

    fixed int, creating recusive output function

--- main.cpp
+++ main.cpp
@@ -16,14 +16,11 @@ typedef vector<dd> vd;
 typedef vector<int> vi;
 
 int globalInnov = 0; 
+dd maxRandom = 100;
 
 double relu(double x){
   return max(0.0,x);
 }
-
-// -1 = input
-// 0 = hidden
-// 1 = output
 
 class Node{
   public:
@@ -34,13 +31,13 @@ class Node{
     dd sum; 
     dd bias; 
   public:
-    Node(int type, int id, vi prevNodes, dd bias, int activ = 0){
-      type = type; 
-      id = id; 
-      prevNodes = prevNodes; 
-      activ = activ; 
-      bias = bias;
-      sum = bias;
+    Node(int Type, int Id, vi PrevNodes, dd Bias, int Activ = 0, int Sum = 0){
+      type = Type; 
+      id = Id; 
+      prevNodes = PrevNodes; 
+      activ = Activ; 
+      bias = Bias;
+      sum = Sum+bias;
     }
     void debug_print(){
       cout << "ID: " << id << endl;
@@ -63,12 +60,12 @@ class Connection{
     int to; 
     dd weight; 
   public:
-    Connection(int innov, int from, int to, dd weight){
+    Connection(int From, int To, dd Weight){
       //FIX INNOV NUMBER
       innov = globalInnov; //globalInnov starts at 0.
-      from = from;
-      to = to;
-      weight = weight;
+      from = From;
+      to = To;
+      weight = Weight;
     }
     void debug_print(){
       cout << "INNOV: " << innov << endl;
@@ -85,22 +82,23 @@ class Agent{
     vector<Node> nodes;
     vector<Connection> connections;
   public:
-    Agent(int numIn, int numOut){
+    Agent(int numIn, int numOut, vd inputs){
       srand((unsigned)time(NULL));
       vi previous; // previous nodes for output
       for (int i=0; i<numIn; i++){
-        nodes.push_back(Node(-1, i, {}, 0)); 
+        nodes.push_back(Node(-1, i, {}, 0, 0, inputs[i])); 
         previous.push_back(i); 
       }
       for (int i=0; i<numOut; i++){
-        dd bias = dd(rand());
-        nodes.push_back(Node(1, i+numIn, previous, bias)); 
+        dd bias = double(rand())*maxRandom/(double)RAND_MAX;
+        nodes.push_back(Node(1, i+numIn, previous, bias, 0, 0)); 
+        // int type, int id, vi prevNodes, dd bias, int activ = 0, int sum = 0
       }
 
       for (int i=0; i<numIn; i++){
         for (int j=0; j<numOut; j++){
-          dd weight = dd(rand());
-          connections.push_back(Connection(globalInnov, i, j, weight));
+          dd weight = double(rand())*maxRandom/(double)RAND_MAX;
+          connections.push_back(Connection(i, j, weight));
           globalInnov++;
         }
       }
@@ -108,23 +106,53 @@ class Agent{
 
     void debug_print(){
       cout << "NODES" << endl;
-      // for (auto const&node:nodes){
-      //   node.debug_print();
-      //   cout << endl;
-      // }
-      // for (auto const&conn:connections){
-      //   conn.debug_print();
-      //   cout << endl;
-      // }
-      for (int i=0; i<nodes.size(); i++){
-        nodes[i].debug_print();
+      for (auto node:nodes){
+        node.debug_print();
         cout << endl;
       }
-      for (int i=0; i<connections.size(); i++){
-        connections[i].debug_print();
+      cout << "CONNECTIONS" << endl;
+      for (auto conn:connections){
+        conn.debug_print();
         cout << endl;
       }
     }
+
+    // vd run_ntwrk(vd inputs){
+    //   for (int i=0; i<numIn){
+    //     nodes[i].sum = inputs[i]; 
+    //   }
+    //   for 
+    // }
+
+    void get_sums(int id){
+      Node curNode = nodes[id];
+      for (auto vertex:curNode.prevNodes){
+        if (vertex.type != -1){ //else we've reached the end
+          get_sums(vertex);
+          curNode.sum += vertex.sum;
+        } else {
+          curNode.sum += vertex.sum;
+        }
+      }
+    }
+
+    // for (i in outputs){
+    //   push f(i.pre)
+    // }
+    // return list
+
+    // f(int pre) {
+    //   int sum
+    //   if pre.leng is 0{
+    //     return sum
+    //   } else {
+    //     for ( i in pre) {
+    //       sum += f(i.pre)
+    //     }
+    //   }
+    //   return activation(sum)
+    // }
+
     //Find out what nodes go to a given node
 
 };
@@ -132,6 +160,9 @@ class Agent{
 
 int main() {
   cout << "\n";
-  Agent FBIagent = Agent(3,2);
+  Agent FBIagent = Agent(3,2, {0,0,1});
   FBIagent.debug_print();
+  FBIagent.get_sums();
+  // Node X = Node(-1, 10, {}, 0);
+  // X.debug_print();
 }
