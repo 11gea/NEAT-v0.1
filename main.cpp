c8acb7c0a4b2c92dd7a58a2c501a8503a30304c2
11gea <21geajustin@gmail.com>
2020-04-14 18:00:48 +0000

    Initializing nodes and connections

--- main.cpp
+++ main.cpp
@@ -13,19 +13,59 @@ typedef long long ll;
 typedef double dd;
 typedef vector<ll> vll;
 typedef vector<dd> vd;
+typedef vector<int> vi;
+
+int globalInnov = 0; 
 
 double relu(double x){
   return max(0.0,x);
 }
 
-class Genome{
-  private:
-    vector<vector<vector<double>>> weights;
-    vector<vector<double>> biases;
-    vector<int> shape;
-    int mark;
+// -1 = input
+// 0 = hidden
+// 1 = output
+
+class Node{
   public:
-    NeuralNetwork(vector<vector<vector<double>>> w, vector<vector<double>> b, vector<int> s, int m) {weights = w; biases = b; shape = s; mark = m;}
+    int type; //-1 = input, 0 = hidden, 1 = output
+    int id; //id of node
+    vi prevNodes; 
+    int activ;
+    dd sum; 
+    dd bias; 
+  public:
+    Node(int type, int id, vi prevNodes, dd bias, int activ = 0){
+      type = type; 
+      id = id; 
+      prevNodes = prevNodes; 
+      activ = activ; 
+      bias = bias;
+      sum = bias;
+    }
+};
+
+class Connection{
+  public:
+    int innov;
+    int from;  
+    int to; 
+    dd weight; 
+  public:
+    Connection(int innov, int from, int to, dd weight){
+      //FIX INNOV NUMBER
+      innov = globalInnov; //globalInnov starts at 0.
+      from = form;
+      to = to;
+      weight = 0;
+    }
+};
+
+class Agent{
+  public:
+    vector<Node> nodes;
+    vector<Conn
+  public:
+    
 };
 
 
